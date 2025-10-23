c8a06f1195e073bcb0267c6d84282b0b316c52ab
Yesmurat Sagyndyk <yesmurat2006@gmail.com>
2025-10-23 14:33:35 -0400

    kjalksdjfkdj

--- datapath.sv
+++ datapath.sv
@@ -17,6 +17,10 @@ module datapath (
     input logic [2:0] funct3D,
     input logic jumpRegD,
                 
+
+
+
+                
     // inputs from Hazard Unit
     input logic StallF, StallD,
     input logic FlushD, FlushE,
